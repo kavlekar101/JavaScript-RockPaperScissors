1cc800228f99b95c6de2f2f5a425a23e73bce037
Ray <kavlekar101@gmail.com>
2021-12-29 17:53:02 -0500

    Initial Rock-Paper-Scissors

---
+++ index.js
@@ -0,0 +1,48 @@
+function computerPlay() {
+    let arr = ["Rock", "Paper", "Scissors"];
+    let num = Math.floor(Math.random() * 2);
+    return arr[num];
+}
+
+function playRound(playerSelection, computerSelection) {
+    let str;
+    switch (computerSelection) {
+        case "Rock": 
+            if(playerSelection.toLowerCase() == "rock") {
+                str = "its a tie!";
+            } else if (playerSelection.toLowerCase() == "paper") {
+                str = "player won!";
+            } else {
+                str = "Computer won";
+            }
+        break;
+
+        case "Paper":
+            if (playerSelection.toLowerCase() == "rock") {
+                str = "Computer won";
+            } else if (playerSelection.toLowerCase() == "paper") {
+                str = "its a tie!";
+            } else {
+                str = "player won!";
+            }
+        break;
+
+        case "Scissors":
+            if (playerSelection.toLowerCase() == "rock") {
+                str = "player won!";
+            } else if (playerSelection.toLowerCase() == "paper") {
+                str = "Computer won";
+            } else {
+                str = "its a tie!";
+            }
+        break;
+
+        default: str = "something terribly wrong has happend";
+        break;
+    }
+    return str;
+}
+
+const playerSelection = prompt("Enter your selection.");
+const computerSelection = computerPlay();
+alert(playRound(playerSelection, computerSelection));
